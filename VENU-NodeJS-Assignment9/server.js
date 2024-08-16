81578d99d8b2eba32f8b1c89d621de223652a825
regulavenu44 <regulavenu985@gmail.com>
2024-08-17 01:10:44 +0530

    Update server.js

--- VENU-NodeJS-Assignment9/server.js
+++ VENU-NodeJS-Assignment9/server.js
@@ -9,7 +9,7 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'public')));
 app.get('/',(req,res)=>{
-    res.redirect('/home');
+    res.render('index');
 });
 app.get('/home',(req,res)=>{
     res.render('index');
